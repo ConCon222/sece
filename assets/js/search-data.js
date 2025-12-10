95f0d638c54ca4606b0e5529780722f0c77dbe08
frank-haoming <53552820+frank-haoming@users.noreply.github.com>
2025-12-10 12:48:45 +0000

    Deploying to gh-pages from @ frank-haoming/frank-haoming.github.io@367e68aeed4ece1688823190bd4080fe766601cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-life-line",
+          title: "Life Line",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/lifeline/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
